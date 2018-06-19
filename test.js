651f24586d73a38fc2a54510167f03430b88ac28
elliottabirch <elliottabirch@gmail.com>
2018-06-18 18:04:27 -0700

    start xlsx

--- test.js
+++ test.js
@@ -1,20 +1,4 @@
-const { sendEmail } = require('./util');
-// const SignIn = require('./reports/SignIn');
-const CampPhotos = require('./reports/CampPhotos');
+const XLSX = require('xlsx');
 
-
-exports.handler = ({ queryStringParameters }, context, callback) => {
-  const { startDate, endDate } = queryStringParameters;
-  CampPhotos(startDate, endDate)
-    .through(stream => sendEmail(stream, 'Camp-Photos'))
-    .stopOnError(err => console.log(err))
-    .toArray(arr => callback(null, arr));
-};
-
-
-exports.handler({
-  queryStringParameters: {
-    startDate: '2018-06-10',
-    endDate: '2018-06-20',
-  },
-}, {}, (err, data) => console.log(err, data));
+XLSX
+;
