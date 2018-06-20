c8e57bf46ac556f75063689183275cfc86449afe
elliottabirch <elliottabirch@gmail.com>
2018-06-19 23:24:03 -0400

    get all data

--- test.js
+++ test.js
@@ -11,6 +11,35 @@ const {
   streamSeasons,
 } = require('./data');
 
+const formatSignInData = (data) => {
+  const {
+
+  } = data;
+  return {
+    firstName: 'First Name',
+    lastName: 'Last Name',
+    nameString: 'Names',
+    numberString: 'Numbers',
+    sunIn: 'Sun - In',
+    sunOut: 'Sun - Out',
+    monIn: 'Mon - In',
+    monOut: 'Mon - Out',
+    tuesIn: 'Tues - In',
+    tuesOut: 'Tues - Out',
+    wedIn: 'Wed - In',
+    wedOut: 'Wed - Out',
+    thursIn: 'Thurs - In',
+    thursOut: 'Thurs - Out',
+    friIn: 'Fri - In',
+    friOut: 'Fri - Out',
+    satIn: 'Sat - In',
+    satOut: 'Sat - Out',
+    canAdministerMedicine: 'Allowed To Administer Medicine',
+    shirtSize: 'Shirt Size',
+    unApproved: 'Unapproved',
+  };
+};
+
 streamSeasons({ seasonIds: [] })
   .pluck('sessionIds')
   .flatMap(sessionIds => streamSessions({ sessionIds }))
@@ -37,6 +66,31 @@ streamSeasons({ seasonIds: [] })
           tuition: tuitionsByTuitionId[session.registrationDetail.tuitionId] || {},
         }, session));
     }))
+  .collect()
+  .doto((a) => {
+    console.log(a);
+  })
+
+  .flatMap(sessions => streamPeople({ answerIds: _.map(sessions, 'registrationDetail.personId') })
+    .collect()
+    .flatMap((persons) => {
+      const personsBypersonId = _.keyBy(persons, 'personId');
+      return hl(sessions)
+        .map(session => Object.assign({
+          person: personsBypersonId[session.registrationDetail.personId] || {},
+        }, session));
+    }))
+  .collect()
+  .flatMap(sessions => streamAnswers({ personIds: _.map(sessions, 'registrationDetail.personId') })
+    .collect()
+    .flatMap((answers) => {
+      const answersByPersonId = _.keyBy(answers, 'personId');
+      return hl(sessions)
+        .map(session => Object.assign({
+          answers: answersByPersonId[session.registrationDetail.personId] || {},
+        }, session));
+    }))
+  .map(formatSignInData)
   .doto((a) => {
     console.log(a);
   })
