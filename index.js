bc2aff5992230ca9b4d2733e23c5aa43a990c62c
elliottabirch <elliottabirch@gmail.com>
2018-06-20 00:11:24 -0400

    get family data

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ exports.campPhotos = (req, context, callback) => {
   const { queryStringParameters = {} } = req;
   const { startDate, endDate } = queryStringParameters;
   console.log(req);
-  console.log('starting campPHotos with', startDate, endDate);
+  console.log('starting campPhotos with', startDate, endDate);
   CampPhotos(startDate, endDate)
     .through(stream => sendEmail(stream, 'Sign-In', PHOTO_EMAIL_GROUP))
     .stopOnError(err => console.log(err))
--- test.js
+++ test.js
@@ -9,34 +9,42 @@ const {
   streamAnswers,
   streamTuitions,
   streamSeasons,
+  streamFamilies,
 } = require('./data');
 
 const formatSignInData = (data) => {
   const {
-
+    person = {},
+    answers = {},
+    tuition = { name: tuitionName },
+    registrationDetail = {},
+    location: locationName,
+    name: sessionName,
+    startDate: { day: startDay, month: startMonth, year: startYear },
+    endDate: { day: endDay, month: endMonth, year: endYear },
+    family = [],
   } = data;
+  const { firstName, lastName } = person;
   return {
-    firstName: 'First Name',
-    lastName: 'Last Name',
-    nameString: 'Names',
-    numberString: 'Numbers',
-    sunIn: 'Sun - In',
-    sunOut: 'Sun - Out',
-    monIn: 'Mon - In',
-    monOut: 'Mon - Out',
-    tuesIn: 'Tues - In',
-    tuesOut: 'Tues - Out',
-    wedIn: 'Wed - In',
-    wedOut: 'Wed - Out',
-    thursIn: 'Thurs - In',
-    thursOut: 'Thurs - Out',
-    friIn: 'Fri - In',
-    friOut: 'Fri - Out',
-    satIn: 'Sat - In',
-    satOut: 'Sat - Out',
-    canAdministerMedicine: 'Allowed To Administer Medicine',
-    shirtSize: 'Shirt Size',
-    unApproved: 'Unapproved',
+    firstName,
+    lastName,
+    sunIn,
+    sunOut,
+    monIn,
+    monOut,
+    tuesIn,
+    tuesOut,
+    wedIn,
+    wedOut,
+    thursIn,
+    thursOut,
+    friIn,
+    friOut,
+    satIn,
+    satOut,
+    canAdministerMedicine,
+    shirtSize,
+    unApproved,
   };
 };
 
@@ -70,8 +78,7 @@ streamSeasons({ seasonIds: [] })
   .doto((a) => {
     console.log(a);
   })
-
-  .flatMap(sessions => streamPeople({ answerIds: _.map(sessions, 'registrationDetail.personId') })
+  .flatMap(sessions => streamPeople({ personIds: _.map(sessions, 'registrationDetail.personId') })
     .collect()
     .flatMap((persons) => {
       const personsBypersonId = _.keyBy(persons, 'personId');
@@ -79,6 +86,24 @@ streamSeasons({ seasonIds: [] })
         .map(session => Object.assign({
           person: personsBypersonId[session.registrationDetail.personId] || {},
         }, session));
+    }))
+  .collect()
+  .flatMap(sessions => streamFamilies({ personIds: [] })
+    .collect()
+    .flatMap(families => streamPeople({ personIds: _.map(families, 'personId') })
+      .collect()
+      .flatMap((persons) => {
+        const personsBypersonId = _.keyBy(persons, 'personId');
+        return hl(families)
+          .map(family => Object.assign({
+            person: personsBypersonId[family.personId] || {},
+          }, family));
+      }))
+    .collect()
+    .flatMap((families) => {
+      const familiesByFamilyId = _.groupBy(families, 'familyId');
+      return hl(sessions)
+        .map(session => Object.assign({ family: familiesByFamilyId[session.person.familyId] }, session));
     }))
   .collect()
   .flatMap(sessions => streamAnswers({ personIds: _.map(sessions, 'registrationDetail.personId') })
@@ -96,3 +121,33 @@ streamSeasons({ seasonIds: [] })
   })
   .stopOnError(err => console.log(err))
   .done(() => console.log('done'));
+
+// firstName: 'First Name',
+// lastName: 'Last Name',
+// primaryName: 'Primary Parent Name',
+// primaryNumber: 'Primary Parent Number',
+// secondaryName: 'secondary Parent Name',
+// secondaryNumber: 'secondary Parent Number',
+// p1Name: 'p1 Parent Name',
+// p1Number: 'p1 Parent Number',
+// p2Name: 'p2 Parent Name',
+// p2Number: 'p2 Parent Number',
+// p3Name: 'p3 Parent Name',
+// p3Number: 'p3 Parent Number',
+// sunIn: 'Sun - In',
+// sunOut: 'Sun - Out',
+// monIn: 'Mon - In',
+// monOut: 'Mon - Out',
+// tuesIn: 'Tues - In',
+// tuesOut: 'Tues - Out',
+// wedIn: 'Wed - In',
+// wedOut: 'Wed - Out',
+// thursIn: 'Thurs - In',
+// thursOut: 'Thurs - Out',
+// friIn: 'Fri - In',
+// friOut: 'Fri - Out',
+// satIn: 'Sat - In',
+// satOut: 'Sat - Out',
+// canAdministerMedicine: 'Allowed To Administer Medicine',
+// shirtSize: 'Shirt Size',
+// unApproved: 'Unapproved',
