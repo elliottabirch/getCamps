d451c5ef3a363d713cc567ddded67c4ede6ec9b2
elliottabirch <elliottabirch@gmail.com>
2018-06-19 23:06:34 -0400

    assign tuition, registration,

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 const XLSX = require('xlsx');
+const _ = require('lodash');
+const hl = require('highland');
 
 const {
   streamSessions,
@@ -11,7 +13,30 @@ const {
 
 streamSeasons({ seasonIds: [] })
   .pluck('sessionIds')
-  .flatMap((sessionIds) => streamSessions({ sessionIds }))
+  .flatMap(sessionIds => streamSessions({ sessionIds }))
+  .collect()
+  .flatMap(sessions => streamRegistrations({ sessionIds: _.map(sessions, 'sessionId') })
+    .collect()
+    .flatMap((registrations) => {
+      const registrationsBySessionId = _.keyBy(registrations, 'sessionId');
+      return hl(sessions)
+        .map(session => Object.assign({
+          registration: registrationsBySessionId[session.sessionId] || null,
+        }, session))
+        .filter(({ registration }) => !!registration);
+    }))
+  .flatMap(session => hl(session.registration.registrationDetails || [])
+    .map(registrationDetail => Object.assign({ registrationDetail: registrationDetail || {} }, session)))
+  .collect()
+  .flatMap(sessions => streamTuitions({ tuitionIds: _.map(sessions, 'registrationDetail.tuitionId') })
+    .collect()
+    .flatMap((tuitions) => {
+      const tuitionsByTuitionId = _.keyBy(tuitions, 'tuitionId');
+      return hl(sessions)
+        .map(session => Object.assign({
+          tuition: tuitionsByTuitionId[session.registrationDetail.tuitionId] || {},
+        }, session));
+    }))
   .doto((a) => {
     console.log(a);
   })
